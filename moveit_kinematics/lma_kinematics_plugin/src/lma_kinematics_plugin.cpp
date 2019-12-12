442760cdb72729266081150ab6913795a4d119b7
JafarAbdi <cafer.abdi@gmail.com>
2019-12-12 20:26:47 +0300

    Merge fixups

--- moveit_kinematics/lma_kinematics_plugin/src/lma_kinematics_plugin.cpp
+++ moveit_kinematics/lma_kinematics_plugin/src/lma_kinematics_plugin.cpp
@@ -210,9 +210,10 @@ bool LMAKinematicsPlugin::obeysLimits(const Eigen::VectorXd& values) const
   return true;
 }
 
-bool LMAKinematicsPlugin::searchPositionIK(const geometry_msgs::Pose& ik_pose, const std::vector<double>& ik_seed_state,
-                                           double timeout, const std::vector<double>& consistency_limits,
-                                           std::vector<double>& solution, const IKCallbackFn& solution_callback,
+bool LMAKinematicsPlugin::searchPositionIK(const geometry_msgs::msg::Pose& ik_pose,
+                                           const std::vector<double>& ik_seed_state, double timeout,
+                                           const std::vector<double>& consistency_limits, std::vector<double>& solution,
+                                           const IKCallbackFn& solution_callback,
                                            moveit_msgs::msg::MoveItErrorCodes& error_code,
                                            const kinematics::KinematicsQueryOptions& options) const
 {

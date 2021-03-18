9f10fcf5b9ff84787a21edb0250ae5380faf16fb
AndyZe <zelenak@picknik.ai>
2021-03-18 10:39:41 -0500

    Use Reflexxes for limit checks. It compiles but does not run.

--- moveit_ros/moveit_servo/include/moveit_servo/servo_calcs.h
+++ moveit_ros/moveit_servo/include/moveit_servo/servo_calcs.h
@@ -162,6 +162,9 @@ protected:
   void suddenHalt(sensor_msgs::msg::JointState& joint_state,
                   const std::vector<const moveit::core::JointModel*>& joints_to_halt) const;
 
+  /** \brief  Scale the delta theta to match joint velocity/acceleration limits */
+  bool enforceVelAccelLimitsWithReflexxes(Eigen::ArrayXd& delta_theta);
+
   /** \brief Avoid overshooting joint limits
       \return Vector of the joints that would move farther past position margin limits
    */
@@ -178,8 +181,8 @@ protected:
   void composeJointTrajMessage(const sensor_msgs::msg::JointState& joint_state,
                                trajectory_msgs::msg::JointTrajectory& joint_trajectory);
 
-  /** \brief Set the filters to the specified values */
-  void resetLowPassFilters(const sensor_msgs::msg::JointState& joint_state);
+  /** \brief Reset current state of Reflexxes to the specified position. Assume zero velocities/accelerations. */
+  void resetReflexxesState(const sensor_msgs::msg::JointState& joint_state);
 
   /** \brief Handles all aspects of the servoing after the desired joint commands are established
    * Joint and Cartesian calcs feed into here
@@ -274,7 +277,7 @@ protected:
   bool wait_for_servo_commands_ = true;
 
   // Flag saying if the filters were updated during the timer callback
-  bool updated_filters_ = false;
+  bool updated_reflexxes_state_ = false;
 
   // Nonzero status flags
   bool have_nonzero_twist_stamped_ = false;
@@ -362,6 +365,7 @@ protected:
   // Use Reflexxes for command smoothing
   std::unique_ptr<ReflexxesAPI> reflexxes_;
   std::unique_ptr<RMLPositionInputParameters> reflexxes_position_input_param_;
-  std::unique_ptr<RMLPositionOutputParameters> reflexxes_position_output_param_;
+  std::shared_ptr<RMLPositionOutputParameters> reflexxes_position_output_param_;
+  RMLPositionFlags reflexxes_flags_;
 };
 }  // namespace moveit_servo

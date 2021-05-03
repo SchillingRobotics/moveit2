727c2ec8d17bfa6787ad4934e1f5ebab0fb341e8
AndyZe <zelenak@picknik.ai>
2021-05-03 16:37:54 -0500

    Fix joint limit handling when velocities aren't included in current_state

--- moveit_core/robot_state/include/moveit/robot_state/robot_state.h
+++ moveit_core/robot_state/include/moveit/robot_state/robot_state.h
@@ -1506,6 +1506,7 @@ public:
   }
   bool satisfiesPositionBounds(const JointModel* joint, double margin = 0.0) const
   {
+    std::cout << joint->satisfiesPositionBounds(getJointPositions(joint), margin) << std::endl;
     return joint->satisfiesPositionBounds(getJointPositions(joint), margin);
   }
   bool satisfiesVelocityBounds(const JointModel* joint, double margin = 0.0) const
